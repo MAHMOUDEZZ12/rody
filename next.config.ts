2154210a43fa894259ec99eee89348e9e94b61df
Mahmoud Ezz <Mu7moud@gmail.com>
2025-08-11 19:15:59 +0000

    fix this erorr please -- if it's because of the api we need to fix
    [23

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   serverActions: {
     bodySizeLimit: '2mb',
-    serverActions: true,
   },
   /* config options here */
   typescript: {
